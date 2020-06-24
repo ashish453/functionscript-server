30a2d6c3cabdb6df6d38a66c2e577684fea00a8c
ashish453 <63253424+ashish453@users.noreply.github.com>
2020-06-24 16:25:33 +0545

    Update __main__.js

--- functions/__main__.js
+++ functions/__main__.js
@@ -1,3 +1,13 @@
+const fs = require('fs');
+const PAGE = fs.readFileSync('./pages/index.html').toString();
+
+/**
+* This API endpoint simply serves a static HTML page.
+*   It replaces all instances of `{%varname}` with corresponding GET / POST
+*   parameters. The default is to respond to the `name` parameter
+* @param {string} name A name to enter.
+* @returns {object.http} response The HTTP response
+*/
 var authConfig = {
   "siteName": "GoIndex - Drive", // Site Name
   "version" : "2.4", // version
